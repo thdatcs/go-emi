5a9ba6108cfb9d376669263cf91fecbceb6b345e
BUI THANH DAT <datbt@ocb.com.vn>
2022-02-28 21:36:38 +0700

    Update

--- src/go-emi/main.go
+++ src/go-emi/main.go
@@ -14,7 +14,7 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 
 	fmt.Print("Input loan amount (VND): ")
-	principalAmountStr, _ := reader.ReadString('\n')
+	loanAmountStr, _ := reader.ReadString('\n')
 	fmt.Print("Input interest rate (365 days): ")
 	interestRateStr, _ := reader.ReadString('\n')
 	fmt.Print("Input loan tenure (months): ")
@@ -29,17 +29,31 @@ func main() {
 	// loanTenureStr := "12"
 	// firstEffectiveDateStr := "20210922"
 	// firstDueDateStr := "20211016"
-	principalAmount, _ := strconv.ParseFloat(strings.ReplaceAll(principalAmountStr, "\n", ""), 64)
+	loanAmount, _ := strconv.ParseFloat(strings.ReplaceAll(loanAmountStr, "\n", ""), 64)
 	interestRate, _ := strconv.ParseFloat(strings.ReplaceAll(interestRateStr, "\n", ""), 64)
 	loanTenure, _ := strconv.Atoi(strings.ReplaceAll(loanTenureStr, "\n", ""))
 	firstEffectiveDate, _ := time.Parse("20060102", strings.ReplaceAll(firstEffectiveDateStr, "\n", ""))
 	firstDueDate, _ := time.Parse("20060102", strings.ReplaceAll(firstDueDateStr, "\n", ""))
+	portionAmount := calculatePortionAmount(loanAmount, interestRate, loanTenure, firstEffectiveDate, firstDueDate)
+	detailPlans := calculateDetailPlans(loanAmount, interestRate, loanTenure, firstEffectiveDate, firstDueDate, portionAmount)
+	fmt.Println(fmt.Sprintf("Loan Amount: %.2f", loanAmount))
+	fmt.Println(fmt.Sprintf("Interest Rate: %.2f", interestRate))
+	fmt.Println(fmt.Sprintf("Portion Amount: %.2f", portionAmount))
+	fmt.Println()
+	fmt.Println("No.\tEffective Date\tDue Date\tPortion Interest\tPortion Loan Amount\tRemaining Loan Amount")
+	for i := 0; i < len(detailPlans); i++ {
+		fmt.Println(fmt.Sprintf("%d\t%s\t%s\t%s\t\t%s\t\t%s", i+1, detailPlans[i]["Effective Date"], detailPlans[i]["Due Date"], detailPlans[i]["Portion Interest Amount"], detailPlans[i]["Portion Loan Amount"], detailPlans[i]["Remaining Loan Amount"]))
+
+	}
+}
+
+func calculatePortionAmount(loanAmount, interestRate float64, loanTenure int, firstEffectiveDate, firstDueDate time.Time) float64 {
 	var effectiveDate time.Time
 	var dueDate time.Time
 	var days float64
 	var portionInterestRate float64
 	var pi float64 = 1.0
-	var sigma float64
+	var sigma float64 = 0.0
 	var portionAmount float64
 	for i := 0; i < loanTenure; i++ {
 		if i == 0 {
@@ -54,14 +68,19 @@ func main() {
 		pi = pi * 1 / (1 + portionInterestRate)
 		sigma = sigma + pi
 	}
-	fmt.Println(fmt.Sprintf("Loan Amount: %.2f", principalAmount))
-	fmt.Println(fmt.Sprintf("Interest Rate: %.2f", interestRate))
-	portionAmount = math.Round(principalAmount / sigma)
-	fmt.Println(fmt.Sprintf("Portion Amount: %.2f", portionAmount))
-	fmt.Println()
+	portionAmount = math.Round(loanAmount / sigma)
+	return portionAmount
+}
+
+func calculateDetailPlans(loanAmount, interestRate float64, loanTenure int, firstEffectiveDate, firstDueDate time.Time, portionAmount float64) []map[string]string {
+	var effectiveDate time.Time
+	var dueDate time.Time
+	var days float64
+	var portionInterestRate float64
 	var portionInterestAmount float64
-	var remainingPrincipalAmount = principalAmount
-	fmt.Println("No.\tEffective Date\tDue Date\tPortion Interest\tPortion Loan Amount\tRemaining Loan Amount")
+	var prevRemainingLoanAmount float64
+	var remainingLoanAmount = loanAmount
+	var detailPlans []map[string]string
 	for i := 0; i < loanTenure; i++ {
 		if i == 0 {
 			effectiveDate = firstEffectiveDate
@@ -72,8 +91,16 @@ func main() {
 		}
 		days = dueDate.Sub(effectiveDate).Hours() / 24
 		portionInterestRate = interestRate * days / 36500
-		portionInterestAmount = math.Round(remainingPrincipalAmount * portionInterestRate)
-		fmt.Println(fmt.Sprintf("%d\t%s\t%s\t%.2f\t\t%.2f\t\t%.2f", i+1, effectiveDate.Format("2006-01-02"), dueDate.Format("2006-01-02"), portionInterestAmount, math.Min(portionAmount-portionInterestAmount, remainingPrincipalAmount), remainingPrincipalAmount))
-		remainingPrincipalAmount = remainingPrincipalAmount - portionAmount + portionInterestAmount
+		portionInterestAmount = math.Round(remainingLoanAmount * portionInterestRate)
+		prevRemainingLoanAmount = remainingLoanAmount
+		remainingLoanAmount = remainingLoanAmount - portionAmount + portionInterestAmount
+		var detailPlan = make(map[string]string)
+		detailPlan["Effective Date"] = effectiveDate.Format("2006-01-02")
+		detailPlan["Due Date"] = dueDate.Format("2006-01-02")
+		detailPlan["Portion Interest Amount"] = fmt.Sprintf("%.2f", portionInterestAmount)
+		detailPlan["Portion Loan Amount"] = fmt.Sprintf("%.2f", math.Min(portionAmount-portionInterestAmount, prevRemainingLoanAmount))
+		detailPlan["Remaining Loan Amount"] = fmt.Sprintf("%.2f", math.Max(0, remainingLoanAmount))
+		detailPlans = append(detailPlans, detailPlan)
 	}
+	return detailPlans
 }
